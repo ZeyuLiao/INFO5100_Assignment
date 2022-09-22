ed2b8944079887d9be3123ef259b908b974ac0cc
Zeyu Liao <liao.ze@northeastern.edu>
2022-09-21 23:18:06 -0400

    Firt Version release

---
+++ Assignment_1/src/Model/Profile.java
@@ -0,0 +1,197 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author ZeyuLiao
+ */
+public class Profile {
+    private String firstName;
+    private String lastName;
+    private String dOB;
+    private String age;
+    private String tel;
+    private String email;
+    private String sl1;
+    private String sl2;
+    private String city;
+    private String country;
+    private String affiliation;
+    private String major;
+    private String career;
+    private String dg1;
+    private String start1;
+    private String end1;
+    private String dg2;
+    private String start2;
+    private String end2;   
+    private ImageIcon img;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getdOB() {
+        return dOB;
+    }
+
+    public void setdOB(String dOB) {
+        this.dOB = dOB;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSl1() {
+        return sl1;
+    }
+
+    public void setSl1(String sl1) {
+        this.sl1 = sl1;
+    }
+
+    public String getSl2() {
+        return sl2;
+    }
+
+    public void setSl2(String sl2) {
+        this.sl2 = sl2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAffiliation() {
+        return affiliation;
+    }
+
+    public void setAffiliation(String affiliation) {
+        this.affiliation = affiliation;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getCareer() {
+        return career;
+    }
+
+    public void setCareer(String career) {
+        this.career = career;
+    }
+
+    public String getDg1() {
+        return dg1;
+    }
+
+    public void setDg1(String dg1) {
+        this.dg1 = dg1;
+    }
+
+    public String getStart1() {
+        return start1;
+    }
+
+    public void setStart1(String start1) {
+        this.start1 = start1;
+    }
+
+    public String getEnd1() {
+        return end1;
+    }
+
+    public void setEnd1(String end1) {
+        this.end1 = end1;
+    }
+
+    public String getDg2() {
+        return dg2;
+    }
+
+    public void setDg2(String dg2) {
+        this.dg2 = dg2;
+    }
+
+    public String getStart2() {
+        return start2;
+    }
+
+    public void setStart2(String start2) {
+        this.start2 = start2;
+    }
+
+    public String getEnd2() {
+        return end2;
+    }
+
+    public void setEnd2(String end2) {
+        this.end2 = end2;
+    }
+
+    public ImageIcon getImg() {
+        return img;
+    }
+
+    public void setImg(ImageIcon img) {
+        this.img = img;
+    }
+    
+    
+    
+}
---
+++ LabDemo2/src/Model/VitalSigns.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author ZeyuLiao
+ */
+public class VitalSigns {
+
+    /**
+     * @param args the command line arguments
+     */
+    private double temprature;
+    private double temprature;
+    private int pulse;
+    private String date;
+    
+    
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
